f39257c7acd2064805fa6db75469988106a0600c
TheResilientCoder <syedasim2021@gmail.com>
2025-03-04 00:51:02 +0500

    4-April-25 intro-to js

---
+++ JS-Web-dev/script.js
@@ -0,0 +1,20 @@
+alert("WELCOME TO JAVA-SCRIPT")
+
+console.log("This cnsole is RUNNIG")
+
+var a =  prompt("Enter some random number")
+var is = confirm ("Are you sure you want to leave this window")
+
+
+if (is){
+    console.log("BYE BYE GOOD LUCK")
+}
+else{
+    console.log("WOW YOU STAYED");
+}
+console.log("Your number is " + a)
+
+document.title = ("JS")
+document.body.style.backgroundColor = "pink";
+// npm means node package manager
+// helps in manages of packeges in node js 
