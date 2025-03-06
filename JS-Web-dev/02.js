a414dec17e94f5631276d1337715ca2c525a4073
TheResilientCoder <syedasim2021@gmail.com>
2025-03-06 07:47:30 +0500

    6- april Learning Js

---
+++ JS-Web-dev/02.js
@@ -0,0 +1,85 @@
+// console.log("Hello my name is asim")
+// var a = 2
+// var b = 4
+// var c = 9
+// console.log(a + b + c)
+// let name = ("Syed asim bacha")
+// console.log(name)
+// // const is Global variable Declaration
+// const Employee = ("Teacher")
+// console.log(Employee)
+// console.log(typeof Employee)
+
+// Diffenrence Between var and let is that let/const  is Block Scope variable
+//  declaration where we want to use some value upto/into specofic Block
+
+// while the var is for global  variable decalration  just like const
+// let a = 20 
+// console.log(a)
+
+// {
+//     let a =  30 
+//     console.log(a)
+// }
+// console.log (a)
+
+
+// var b = 20 
+// console.log(b)
+
+// {
+//     var b  =  30 
+//     console.log(b)
+// }
+
+// console.log (b)
+
+
+// const c = 20 
+// console.log(c)
+
+// {
+//     const c   =  30 
+//     console.log(c)
+// }
+//console.log (c)
+
+
+// DATA TYPES IN JS String/ NUmber / BigInt/ null / undefined/ boolean
+let x 
+let y = ("Asim")
+let z = 211212
+let a = null
+let b = 34444444444444444
+let c = 0
+
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(a)
+console.log(b)
+console.log(c)
+
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof z)
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+// Null is of type object because and in specificcation it was declared as 
+// Null now there is alot of code which relie on JS null thats why it cannt 
+// be chnaged 
+
+// Object in JS
+let o = {
+    "Student_name" : "Syed Asim Bacha",
+    "Phone_Number" : "0313-1935528",
+    Marks : 502
+}
+console.log(o)
+o.newMarks = 969;
+console.log(o)
+
+o.NewStudent_name = "Syed Awais Ali shah"
+console.log(o)
